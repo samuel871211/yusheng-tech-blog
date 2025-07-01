174f96baeff8cb1d29ff405716af5dd277e1e676
yusheng <samuel871211@gmail.com>
2025-07-01 09:43:26 +0800

    feat: http-request-method-1 完成

---
+++ docs/http/http-request-methods-2.md
@@ -0,0 +1,15 @@
+---
+title: HTTP Request Methods (下篇)
+description: HTTP Request Methods (下篇)
+---
+
+## OPTIONS
+
+## TRACE
+
+## 參考資料
+
+- https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Methods/OPTIONS
+- https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Methods/TRACE
+- https://httpwg.org/specs/rfc9110.html#rfc.section.9.3.2
+- https://fetch.spec.whatwg.org/#methods
--- sidebars.ts
+++ sidebars.ts
@@ -44,7 +44,8 @@ const sidebars: SidebarsConfig = {
         "http/strict-transport-security",
         "http/upgrade",
         "http/http-redirections",
-        "http/http-request-methods",
+        "http/http-request-methods-1",
+        // "http/http-request-methods-2", // 必讀
         // "http/http-content-negotiation", // 必讀
         // "http/http-authentication", // 必讀
         // "http/beyond-cors", // 必讀
